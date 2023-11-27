9ca06cd9cff8c5cb8b6159924fa19c335d5ae564
unknown <kasper1116.kh@gmail.com>
2023-11-27 14:39:01 +0800

    傳遞資料至view顯示

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HelloController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,5 @@ Route::get('/', function () {
 });
 
 Route::get('/home',[HomeController::class,'index'])->name('home.index');
+
+Route::get('/hello/{name?}',[HelloController::class,'index'])->name('hello.index');
